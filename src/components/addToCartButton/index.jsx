98b9614c2525cf050b5412f113ce591ed0fafb34
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-06-06 08:40:19 -0500

    Disable add to cart button when user is not logged in

--- src/components/addToCartButton/index.jsx
+++ src/components/addToCartButton/index.jsx
@@ -2,9 +2,11 @@ import React, { useContext, useState } from 'react';
 import { ShoppingCartContext } from '../../contexts/ShoppingCartContext';
 import Modal from '../cartModalPopup/index';
 import './styles.css';
+import { AuthContext } from '../../contexts/AuthContext';
 
 
 export default function AddToCartButton({ product }) {
+    const { user } = useContext(AuthContext);
     const { addToCart } = useContext(ShoppingCartContext);
     const [showModalPopup, setShowModalPopup] = useState(false);
 
@@ -19,8 +21,8 @@ export default function AddToCartButton({ product }) {
 
     return (
         <>
-            <button className='add-to-cart-button' onClick={handleClick}>
-                Add to Cart <img className='add-to-cart-icon' src={`${import.meta.env.VITE_REACT_APP_BASE_URL}/src/img/plus-circle.svg`} />
+            <button className={`add-to-cart-button ${!user && 'disabled'}`} onClick={handleClick} disabled={!user}>
+                Add to Cart <img className={`add-to-cart-icon ${!user && 'disabled'}`} src={`${import.meta.env.VITE_REACT_APP_BASE_URL}/src/img/plus-circle.svg`} />
             </button>
             {showModalPopup && <Modal
                 title={product.title}
