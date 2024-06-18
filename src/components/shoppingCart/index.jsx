4c1a049fa9d263affd607f98362b298a3a4ae4c1
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-06-18 08:17:04 -0400

    Add subtotal to shopping cart, shopping cart styling edits

--- src/components/shoppingCart/index.jsx
+++ src/components/shoppingCart/index.jsx
@@ -25,10 +25,19 @@ export default function ShoppingCart() {
         });
     }
 
+    const totalPrice = cart.reduce((total, item) => total + item.product.price * item.quantity, 0);
+
     return (
         <div className='shopping-cart-container'>
             <h2>Shopping Cart</h2>
             <div className='shopping-cart-items-container'>
+                <div className='shopping-cart-items-header-container'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div>qty:</div>
+                    <div>price:</div>
+                </div>
                 <ul>
                     {cart.map((item, index) => (
                         <li className='cart-item' key={item.product._id || index}>
@@ -36,12 +45,6 @@ export default function ShoppingCart() {
                                 <span>{item.product.title}</span>
                             </div>
                             <div className='cart-item-details'>
-                                <div className='price-container'>
-                                    <span className='details-title'>price:</span>${item.product.price}
-                                </div>
-                                <div className='qty-container'>
-                                    <span className='details-title'>qty:</span> {item.quantity}
-                                </div>
                                 <input
                                     type='number'
                                     min='1'
@@ -50,10 +53,20 @@ export default function ShoppingCart() {
                                     onChange={(e) => handleQuantityChange(item.product._id, e.target.value)}
                                 />
                                 <Button onClick={() => handleRemoveClick(item.product._id)} buttonText={'Remove'} />
+                                <div></div>
+                                <div className='qty-container'>
+                                    {item.quantity}
+                                </div>
+                                <div className='price-container'>
+                                    ${item.product.price}
+                                </div>
                             </div>
                         </li>
                     ))}
                 </ul>
+                <div className='subtotal-container'>
+                    <span className='subtotal-title'>Subtotal ({cart.length} items): </span> <b>${totalPrice.toFixed(2)}</b>
+                </div>
             </div>
             <Button onClick={() => navigate('/checkout')} buttonText={'Proceed to checkout'} />
             <Button onClick={removeAllFromCart} buttonText={'Remove all items from cart'} />
