f9f670cd08c78ec7f4c5e56661551658777c9003
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-09-04 13:43:01 -0400

    Update navBar dropdown menu state name

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -7,12 +7,13 @@ import './styles.css';
 
 export default function NavBar() {
     const { user } = useContext(AuthContext);
-    const [dropdown, setDropdown] = useState(false);
+    const [mainMenuDropdown, setMainMenuDropdown] = useState(false);
+    const [accountMenuDropdown, setAccountMenuDropdown] = useState(false);
 
     const logout = useLogout();
 
-    function handleToggleMenu() {
-        setDropdown(!dropdown);
+    function handleToggleMainMenuDropdown() {
+        setMainMenuDropdown(!mainMenuDropdown);
     }
 
     function handleLogout() {
@@ -51,24 +52,24 @@ export default function NavBar() {
                     }
                 </div>
                 <div className='hamburger-menu-container'>
-                    <div onClick={handleToggleMenu} className='hamburger-menu'>
+                    <div onClick={handleToggleMainMenuDropdown} className='hamburger-menu'>
                         <div className='bar'></div>
                         <div className='bar'></div>
                         <div className='bar'></div>
                     </div>
                 </div>
             </div>
-            <div className={`dropdown ${dropdown ? 'open' : ''}`}>
+            <div className={`dropdown ${mainMenuDropdown ? 'open' : ''}`}>
                 <ul>
-                    <li><Link to="/shop" className='tab dropdownTab' onClick={handleToggleMenu}>SHOP</Link></li>
-                    <li><Link to="/cart" className='tab dropdownTab' onClick={handleToggleMenu}>CART</Link></li>
+                    <li><Link to="/shop" className='tab dropdownTab' onClick={handleToggleMainMenuDropdown}>SHOP</Link></li>
+                    <li><Link to="/cart" className='tab dropdownTab' onClick={handleToggleMainMenuDropdown}>CART</Link></li>
                     {!user
                         ? <>
-                            <li><Link to="/login" className='tab dropdownTab' onClick={handleToggleMenu}>LOGIN</Link></li>
-                            <li><Link to="/signup" className='tab dropdownTab' onClick={handleToggleMenu}>SIGNUP</Link></li>
+                            <li><Link to="/login" className='tab dropdownTab' onClick={handleToggleMainMenuDropdown}>LOGIN</Link></li>
+                            <li><Link to="/signup" className='tab dropdownTab' onClick={handleToggleMainMenuDropdown}>SIGNUP</Link></li>
                         </>
                         : <>
-                            <li><Link to="/profile" className='tab dropdownTab' onClick={handleToggleMenu}>PROFILE</Link></li>
+                            <li><Link to="/profile" className='tab dropdownTab' onClick={handleToggleMainMenuDropdown}>PROFILE</Link></li>
                             <li><Link to="/login" className='tab dropdownTab' onClick={handleLogout}>LOGOUT</Link></li>
                         </>
                     }
